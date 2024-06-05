76a99f2b592e7f96f9cb81d82ae2cca22af6bc7b
bobbomania <gabritro03@gmail.com>
2024-06-05 17:09:41 +0100

    FEAT @gt922: Added photo carousel with correct scrolling

--- drp-frontend/drp/drp-2024/PhotoGrid.js
+++ drp-frontend/drp/drp-2024/PhotoGrid.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Image, StyleSheet, Dimensions, TouchableOpacity, Text } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
+import { View, Image, StyleSheet, Dimensions } from 'react-native';
 import { REMOTE_HOST } from './Config';
-import 'react-native-reanimated';
+import Carousel from 'react-native-reanimated-carousel';
 
 const PhotoGrid = ({ community }) => {
   const [images, setImages] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -28,16 +26,6 @@ const PhotoGrid = ({ community }) => {
   const screenWidth = Dimensions.get('screen').width;
   const imageHeight = Dimensions.get('screen').height * 0.3;
 
-  const handleNext = () => {
-    const nextIndex = (currentIndex + 1) % images.length;
-    setCurrentIndex(nextIndex);
-  };
-
-  const handlePrevious = () => {
-    const prevIndex = (currentIndex - 1 + images.length) % images.length;
-    setCurrentIndex(prevIndex);
-  };
-
   return (
     <View style={styles.container}>
       {images.length > 0 && (
@@ -47,20 +35,16 @@ const PhotoGrid = ({ community }) => {
           height={imageHeight}
           data={[...new Array(images.length).keys()]}
           scrollAnimationDuration={1000}
-          renderItem={({ _ }) => (
+          renderItem={({ index }) => (
             <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-              <Image source={{ uri: images[currentIndex] }} style={[styles.image, { width: screenWidth - 40, height: imageHeight }]} />
+              <Image source={{ uri: images[index] }} style={[styles.image, { width: screenWidth - 40, height: imageHeight }]} />
             </View>
           )}
-          onSnapToItem={(index) => setCurrentIndex(index)}
+          mode="parallax"
+          parallaxScrollingScale={0.9}
+          parallaxScrollingOffset={50}
         />
       )}
-      <TouchableOpacity onPress={handlePrevious} style={[styles.button, styles.buttonLeft]}>
-        <Text style={styles.buttonText}>{'<'}</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleNext} style={[styles.button, styles.buttonRight]}>
-        <Text style={styles.buttonText}>{'>'}</Text>
-      </TouchableOpacity>
     </View>
   );
 };
@@ -75,24 +59,6 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     marginHorizontal: 20,  // Add margin to the image
     borderRadius: 5,
-  },
-  button: {
-    position: 'absolute',
-    top: '50%',
-    transform: [{ translateY: -25 }],
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonLeft: {
-    left: 10,
-  },
-  buttonRight: {
-    right: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 20,  // Increase font size for better visibility
   },
 });
 
