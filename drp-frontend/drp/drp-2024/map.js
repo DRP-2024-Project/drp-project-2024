0bfa73e53b795dad15aa15ab135799093f6ba590
Finlay Moss <fm822@ic.ac.uk>
2024-06-12 21:09:06 +0100

    FEAT: Added functionality to open in apple maps

--- drp-frontend/drp/drp-2024/map.js
+++ drp-frontend/drp/drp-2024/map.js
@@ -1,16 +1,23 @@
 import React from 'react';
-// Import required components
-import {SafeAreaView, StyleSheet, View} from 'react-native';
-// Import Map and Marker
-import MapView, {Marker} from 'react-native-maps';
+import { SafeAreaView, StyleSheet, View, Alert, Linking, Platform, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 
-
-export default SimpleMap = ({route}) => {
+export default SimpleMap = ({ route }) => {
   const { latitude, longitude } = route.params;
   const parsedLatitude = parseFloat(latitude);
   const parsedLongitude = parseFloat(longitude);
+
+  const openMap = () => {
+    const url = Platform.select({
+      ios: `maps:0,0?q=${parsedLatitude},${parsedLongitude}`,
+      android: `geo:0,0?q=${parsedLatitude},${parsedLongitude}`
+    });
+
+    Linking.openURL(url).catch(err => Alert.alert('Error', 'Unable to open map'));
+  };
+
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
         <MapView
           style={styles.mapStyle}
@@ -20,19 +27,20 @@ export default SimpleMap = ({route}) => {
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-          customMapStyle={mapStyle}>
+          customMapStyle={mapStyle}
+        >
           <Marker
-            draggable
             coordinate={{
               latitude: parsedLatitude,
               longitude: parsedLongitude,
             }}
-            onDragEnd={
-              (e) => alert(JSON.stringify(e.nativeEvent.coordinate))
-            }
-            title={'Test Marker'}
-            description={'This is a description of the marker'}
-          />
+          >
+            <Callout onPress={openMap}>
+              <View>
+                <Text>Open in Maps</Text>
+              </View>
+            </Callout>
+          </Marker>
         </MapView>
       </View>
     </SafeAreaView>
@@ -40,83 +48,83 @@ export default SimpleMap = ({route}) => {
 };
 
 const mapStyle = [
-  {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-  {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-  {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+  { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+  { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+  { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
   {
     featureType: 'administrative.locality',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}],
+    stylers: [{ color: '#d59563' }],
   },
   {
     featureType: 'poi',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}],
+    stylers: [{ color: '#d59563' }],
   },
   {
     featureType: 'poi.park',
     elementType: 'geometry',
-    stylers: [{color: '#263c3f'}],
+    stylers: [{ color: '#263c3f' }],
   },
   {
     featureType: 'poi.park',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#6b9a76'}],
+    stylers: [{ color: '#6b9a76' }],
   },
   {
     featureType: 'road',
     elementType: 'geometry',
-    stylers: [{color: '#38414e'}],
+    stylers: [{ color: '#38414e' }],
   },
   {
     featureType: 'road',
     elementType: 'geometry.stroke',
-    stylers: [{color: '#212a37'}],
+    stylers: [{ color: '#212a37' }],
   },
   {
     featureType: 'road',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#9ca5b3'}],
+    stylers: [{ color: '#9ca5b3' }],
   },
   {
     featureType: 'road.highway',
     elementType: 'geometry',
-    stylers: [{color: '#746855'}],
+    stylers: [{ color: '#746855' }],
   },
   {
     featureType: 'road.highway',
     elementType: 'geometry.stroke',
-    stylers: [{color: '#1f2835'}],
+    stylers: [{ color: '#1f2835' }],
   },
   {
     featureType: 'road.highway',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#f3d19c'}],
+    stylers: [{ color: '#f3d19c' }],
   },
   {
     featureType: 'transit',
     elementType: 'geometry',
-    stylers: [{color: '#2f3948'}],
+    stylers: [{ color: '#2f3948' }],
   },
   {
     featureType: 'transit.station',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}],
+    stylers: [{ color: '#d59563' }],
   },
   {
     featureType: 'water',
     elementType: 'geometry',
-    stylers: [{color: '#17263c'}],
+    stylers: [{ color: '#17263c' }],
   },
   {
     featureType: 'water',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#515c6d'}],
+    stylers: [{ color: '#515c6d' }],
   },
   {
     featureType: 'water',
     elementType: 'labels.text.stroke',
-    stylers: [{color: '#17263c'}],
+    stylers: [{ color: '#17263c' }],
   },
 ];
 
@@ -138,3 +146,4 @@ const styles = StyleSheet.create({
     bottom: 0,
   },
 });
+
